8dde902a6fb637e64eebdfd0668d36ab7a7a7405
rbides <renanbides@gmail.com>
2024-10-10 13:46:35 -0300

    env

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = ""
+    DATABASE_URL: str = "postgresql://postgres:postgres2024@mozio-db:5432/postgres"
 
     model_config = SettingsConfigDict(env_file=".env")
 
